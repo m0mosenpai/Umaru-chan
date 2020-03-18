b68e2e02af9735530fb6d155d3243a6721fe3654
Sarthak <sarthakoct@gmail.com>
2020-03-18 19:49:25 +0530

    Added --mal-id functionality - stores credentials in secrets.py

--- source/back_process.py
+++ source/back_process.py
@@ -4,7 +4,7 @@ import os
 import datetime
 import pytz
 from downloader.downloader import items
-from downloader.downloader.spiders import anime_downloader, mal_updater
+from downloader.downloader.spiders import anime_downloader
 
 BUFFSIZE = 2048
 ACTIVE = False
@@ -16,14 +16,18 @@ def getWatchlist():
 		watchlist = file.read().split('\n')
 	return watchlist[:-1]
 
+#PDT time for HorribleSubs
 def getPDT():
-	#PDT time for HorribleSubs
 	pst_timezone = pytz.timezone("US/Pacific")
 	pdt = datetime.datetime.now(pst_timezone).time()
 	return pdt
 
+def closeSocket():
+	
+
 #Opens TCP ipv4 socket on specified port and host
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.bind((socket.gethostname(), 6969))
 s.listen(5)
 
@@ -38,6 +42,7 @@ while True:
 		
 	client_msg = clientsocket.recv(BUFFSIZE).decode('utf-8')
 
+	#If send-status ping is received, IST and PDT is sent along with activity status
 	if client_msg == "send-status":
 		time = "Local Time: {}	PDT: {} \n".format(local_datetime, getPDT())
 		clientsocket.send(bytes(time, 'utf-8'))
@@ -46,9 +51,21 @@ while True:
 		else:
 			clientsocket.send(bytes("All done for the day! \n", 'utf-8'))
 		
+	#If show-watchlist ping is received, watchlist is sent to the client	
 	if client_msg == "show-watchlist":
 		clientsocket.send(bytes("Your Watchlist: \n", 'utf-8'))
 		watchlist = getWatchlist()
 
-	#if client_msg == "refresh-list":
+	#If a login header is found in the message, user and pass is extracted and stored in secrets.py	
+	if client_msg[:5] == "login":
+		u = client_msg[5:].split(':')[0]
+		p = client_msg[5:].split(':')[1]
+
+		with open("secrets.py", 'w') as secrets:
+			secrets.write("_id = \"{}\"\n".format(u))
+			secrets.write("_pass = \"{}\"\n".format(p))
+
+		clientsocket.send(bytes("MAL Login ID set! Check secret.py.\n", 'utf-8'))
+		clientsocket.send(bytes("Auto list-updation is on. Don't forget to add anime to your 'Watching' list on MAL!\n", 'utf-8'))
+	
 		
--- source/client.py
+++ source/client.py
@@ -32,15 +32,19 @@ def refresh(s):
 		s.send(refresh_ping)
 
 def setMAL(s, username, password):
-	#Send login credentials to server
-	cred = 'login' + username + ':' + password
-	s.send(cred.encode('utf-8'))
+	while True:	
+		#Send login credentials to server by prepending login header
+		cred = 'login' + username + ':' + password
+		s.send(cred.encode('utf-8'))
+
+		msg = s.recv(BUFFSIZE).decode('utf-8')
+		print(msg)
 
 parser = argparse.ArgumentParser(description="Command-line interface for Umaru-chan")
 parser.add_argument('-s', '--status', help="Displays current status.",action='store_true')
 parser.add_argument('-w', '--watchlist', help='Displays current set watchlist', action='store_true')
 parser.add_argument('-m', '--mal-id', nargs=2, help="Sets username and password of MyAnimeList account")
-parser.add_argument('-r', '--refresh', help="Refreshes watchlist", action='store_true')
+parser.add_argument('-r', '--refresh', help="Refreshes database", action='store_true')
 args = parser.parse_args()
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
