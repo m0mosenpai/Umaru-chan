9e6f11b61222ee22d455235feeb72e2e4c416019
prog-butter <51019019+prog-butter@users.noreply.github.com>
2020-03-18 14:44:38 +0530

    slight update

--- source/downloader/downloader/spiders/mal_updater.py
+++ source/downloader/downloader/spiders/mal_updater.py
@@ -3,14 +3,16 @@ from scrapy.http import FormRequest
 from scrapy.utils.response import open_in_browser
 
 class MalUpdater(scrapy.Spider):
-    name = 'mal_updater'
-    start_urls = ['https://myanimelist.net/login.php?from=%2F']
+	name = 'mal_updater'
+	start_urls = ['https://myanimelist.net/login.php']
 
-    def parse(self, response):
-    	token = response.xpath('//*[@name="csrf_token"]/@content').extract_first()
-    	print('Token: ' + token)
+	def parse(self, response):
+		open_in_browser(response)
+		#token = response.xpath('//*[@name="csrf_token"]/@content').extract_first()
+		print('Token: ' + token)
 
-    	return FormRequest.from_response(response, formdata={'csrf_token': str(token), 'password': 'Kgggdkp@2609', 'user_name': 'butterMiner', 'cookie': '1', 'sublogin': 'Login', 'submit': '1'}, callback=self.crawl_list)
+		#return FormRequest.from_response(response, formdata={'csrf_token': str(token), 'password': 'Kgggdkp@2609', 'user_name': 'butterMiner', 'cookie': '1', 'sublogin': 'Login', 'submit': '1'}, callback=self.crawl_list)
 
-    def crawl_list(self, response):
-    	open_in_browser(response)
+	def crawl_list(self, response):
+		#open_in_browser(response)
+		print(response.body)
---
+++ source/mal_log.py
@@ -0,0 +1,39 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import secrets
+from time import sleep
+
+class animelist():
+	def __init__(self):
+		chrome_options = Options()
+		#chrome_options.add_argument("--headless")
+		self.driver = webdriver.Chrome(chrome_options=chrome_options)
+		self.driver.maximize_window()
+
+	def login(self):
+		self.driver.get("https://myanimelist.net/login.php?")
+		sleep(5)
+
+		email = self.driver.find_element_by_xpath("""//*[@id="loginUserName"]""")
+		email.send_keys(secrets._id)
+
+		password = self.driver.find_element_by_xpath("""//*[@id="login-password"]""")
+		password.send_keys(secrets._pass)
+
+		sleep(2)
+
+		btn = self.driver.find_element_by_xpath("""//*[@id="dialog"]/tbody/tr/td/form/div/p[6]/input""")
+		btn.click()
+
+	def gotoanime(self, animename):
+		self.driver.get('https://myanimelist.net/animelist/{}'.format(secrets._id))
+
+	def updateanime(self):
+		self.driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+		btn = self.driver.find_element_by_class_name("""//*[@id="list-container"]/div[3]/div/table/tbody[1]/tr[0]/td[6]/div/a/i""")
+		btn.click()
+
+
+bot = animelist()
+bot.login()
+bot.gotoanime(animename='37403/Ahiru_no_Sora')
