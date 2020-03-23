5331d083424d5494b49690b7f76e21317b997ced
Sarthak <sarthakoct@gmail.com>
2020-03-23 18:17:20 +0530

    Deleted mount_part.sh and watch_anime.sh

--- source/mount_part.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#A script to mount Bitlocker Encrypted Windows partition
-
-#Enter path to the partition to be mounted and Recovery Key below
-dislocker /dev/nvme0n1p3 -v --recovery-password=<Enter-Recovery-Key-Here> /media/bitlocker
-mount -o loop /media/bitlocker/dislocker-file /media/bitlockermount
