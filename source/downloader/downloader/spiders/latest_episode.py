f8a86a7d56aee59791fc77a77f8268e097605e8b
prog-butter <51019019+prog-butter@users.noreply.github.com>
2020-04-11 18:13:25 +0530

    Shows approx time until ep release now

--- source/downloader/downloader/spiders/latest_episode.py
+++ source/downloader/downloader/spiders/latest_episode.py
@@ -17,6 +17,8 @@ DONE = False
 
 ANIME_IN_CHECK = ""
 
+schedule = {}
+
 #Return true if latest ep is out, otherwise return false
 def checkLatestEp(response):
 	latest_ep = response.xpath('//td[@colspan="2"]/a[not(@class)]/@title').extract()[0]
@@ -81,7 +83,9 @@ class HSlatestShow(scrapy.Spider):
 		global path
 		global quality
 		global ANIME_IN_CHECK
+		global schedule
 
+		schedule = readSchedule()
 		config = readConfig()
 		path = config["main"]["torrent"]
 		quality = config["main"]["quality"]
@@ -97,6 +101,22 @@ class HSlatestShow(scrapy.Spider):
 				yield scrapy.Request(query, callback = self.parse_show)
 
 	def parse_show(self, response):
+		str = response.request.url
+		str = str[str.find("+") + 1:str.find("1080p") - 1].replace("+", " ")
+
+		curr_pdt = getPDT().strftime("%H:%M")
+		curr_min = int(curr_pdt[0:2]) * 60 + int(curr_pdt[3:5])
+
+		show_time = ""
+		show_min = 0
+		if str in schedule.keys():
+			show_time = schedule[str]
+			show_min = int(show_time[0:2]) * 60 + int(show_time[3:5])
+		diff = 0
+		if show_min > curr_min:
+			diff = show_min - curr_min
+		diff_hr = int(diff / 60)
+		diff_min = diff % 60
 		#Get the latest episode of the anime
 		try:
 			latest_ep = response.xpath('//td[@colspan="2"]/a[not(@class)]/@title').extract()[0]
@@ -110,11 +130,11 @@ class HSlatestShow(scrapy.Spider):
 			downloadEp(epno, aname, response)
 		except IndexError:
 			#First episode of season, hence page will be empty
-			print("\033[91m[-] Empty page\033[0m")
+			print("\033[91m[-] {}: Empty page [Approx {}h{}m]\033[0m".format(str, diff_hr, diff_min))
 
 		if self.mode == "normal":
 			if not checkLatestEp(response):
-				print("[*] Latest episode of \033[95m{}\033[0m is still not out. Waiting.".format(ANIME_IN_CHECK))
+				print("[*] Latest episode of \033[95m{}\033[0m is still not out. Waiting. [Approx {}h{}m]".format(ANIME_IN_CHECK, diff_hr, diff_min))
 				#Latest ep is not out, continue checking
 				config = readConfig()
 				config["watchlist"][ANIME_IN_CHECK][1] = "-1"
